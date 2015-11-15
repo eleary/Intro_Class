2e926209bd6de21340373e5055f93e3b58e43eff
Liz Leary <lizleary@Lizs-MacBook-Air.local>
2015-11-15 14:37:04 -0800

    Adding the survey file forname, color, hobby, movie

---
+++ survey.py
@@ -0,0 +1,8 @@
+def print_survey_results(name, color, hobby, movie):
+	print "My name is "+ name
+	print name +"'s favorite color is "+color
+	print name +"'s favorite hobby is "+hobby
+	print name +"'s favorite movie is "+movie
+print print_survey_results("Liz", "green", "reading", "Space Jam")
+
+
